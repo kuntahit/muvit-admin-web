eda023af13455533404cc7289fabfd1bc2842b23
Paul Akubo <paulakubo6@gmail.com>
2023-05-10 01:00:19 +0100

    chore: initial commit

---
+++ src/modules/App.js
@@ -0,0 +1,3 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Route, Router, browserHistory, Redirect } from 'react-router';
---
+++ src/util/auth.js
@@ -0,0 +1,1 @@
+import SimpleCrypto from 'simple-crypto-js';
